3332a5b66f880005724cd50ac0b06b377737de47
Umar Ahmed <wspuma@gmail.com>
2019-01-31 20:28:58 -0500

    updated regression model - more data - quantified error/results

--- data_mining.py
+++ data_mining.py
@@ -1,13 +1,14 @@
 import praw
 import requests
 import datetime
+import time
 
 # N is the number of posts to be used
 N = 10000
 secret = ""
 username = ""
 clientID = ""
-reddit = praw.Reddit(user_agent='nani (by /u/USERNAME)',
+reddit = praw.Reddit(user_agent=' (by /u/)',
                      client_id=clientID, client_secret=secret,
                      username=username, password='')
 
@@ -17,9 +18,10 @@ r = requests.get(
     'http://www.reddit.com/r/{}.json'.format(subreddit),
     headers={'user-agent': 'Mozilla/5.0'})
 
-
-
-data = [[i.title,i.score, datetime.datetime.fromtimestamp(i.created)] for i in subreddit.hot(limit=N)]
+data = list()
+for _ in range(20):
+    data += [[i.title,i.score, datetime.datetime.fromtimestamp(i.created)] for i in subreddit.hot(limit=500)]
+    time.sleep(1)
 
 
 with open('titles.txt', mode="w") as outfile:  
--- dataframe.py
+++ dataframe.py
@@ -6,25 +6,29 @@ import numpy
 import re
 from sklearn.linear_model import Ridge
 import random
+import _pickle
+#import matplotlib.pyplot as plt
+#from sklearn.model_selection import cross_val_score
+
 
 # ----------- Cleaning, and creating dataframe from text files --------------
 
 with open('titles.txt') as f:
-    titles = f.read().splitlines()
+    titles = [i for i in f]
 
 with open('scores.txt') as f:
-    scores_ = f.read().splitlines()
+    scores = [int(i) for i in f]
 
 with open('times.txt') as f:
-    times = f.read().splitlines()
-
-scores = [int(i) for i in scores_]
-
+    times = [i for i in f]
 
 df = pandas.DataFrame({'titles':titles})
 df['times'] = times
 df['scores'] = scores
 
+# see distribution of upvotes
+#plt.hist([min(int(i), 1000) for i in scores])
+#plt.show()
 
 # ----------- Bag of Words Matrix -----------
 
@@ -34,7 +38,6 @@ matrix = vectorizer.fit_transform(df["titles"])
 
 
 # --------- Reducing Dimensionality --------------
-# gotta reduce size so this doesn't take forever
 
 col = df["scores"].copy(deep=True)
 col_mean = col.mean()
@@ -99,34 +102,50 @@ features = numpy.hstack([non_nlp, meta, chi_matrix.todense()])
 
 
 
-# ------ It's prediction time friends ------------
-# We gonna use ridge regression instead of linear cuz it's cool
+# ------ Actual training/predicting ------------
 
-
-train_rows = 8000
-test_rows = 1000
+train_rows = int(len(titles) // 2)
+#test_rows = 100
 # Set a seed to get the same "random" shuffle every time.
 random.seed(1)
 
-# Shuffle the indices for the matrix.
+# Shuffle the indices for the matrix
 indices = list(range(features.shape[0]))
 random.shuffle(indices)
 
-# Create train and test sets.
+# Create train and test sets
 train = features[indices[:train_rows], :]
 
-
-test = features[indices[:test_rows], :]
+test = features[indices[train_rows:], :]
 
 train_upvotes = df["scores"].iloc[indices[:train_rows]]
 test_upvotes = df["scores"].iloc[indices[train_rows:]]
 train = numpy.nan_to_num(train)
 
 
-# Run the regression and generate predictions for the test set.
-reg = Ridge(alpha=1)
+# Fit the model on training set and predict remaining data 
+reg = Ridge(0.1)
 reg.fit(train, train_upvotes)
 
+#scores = cross_val_score(reg, train, train_upvotes, scoring="neg_mean_squared_error", cv=10)
+#scores = numpy.sqrt(-scores)
+# print scores.mean()
 
-predictions = reg.predict(test)
+# Compare predictions against real values
+pred_upvotes = reg.predict(test)
+actual_upvotes = list(df["scores"])[train_rows:]
+within = [1 for i in range(len(pred_upvotes)) if abs(pred_upvotes[i] - actual_upvotes[i]) < 10]
+print(sum(within), " predictions were within 10 out of", len(actual_upvotes))
+avg_error = sum([abs(pred_upvotes[i] - actual_upvotes[i]) for i in range(len(pred_upvotes))]) / len(pred_upvotes)
+print("avg error was", avg_error)
 
+
+# save the classifier
+with open('my_dumped_classifier.pkl', 'wb') as fid:
+    _pickle.dump(reg, fid)    
+
+'''
+# load it again
+with open('my_dumped_classifier.pkl', 'rb') as fid:
+    gnb_loaded = cPickle.load(fid)
+'''
